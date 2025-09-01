ab5d411e3eafe867e9545696df7f5656590debdf
yuta20253 <magee.20253@gmail.com>
2025-09-01 17:30:02 +0900

    feat: UserFactoryにroleカラムを追加

---
+++ laravel/app/Http/Controllers/Api/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function login (Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => '認証が失敗しました。'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return [
+            'user' => $user,
+            'token' => $token,
+        ];
+    }
+}
