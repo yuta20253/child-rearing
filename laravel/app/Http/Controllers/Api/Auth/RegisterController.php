a8ea2989e941350ebbde7c5adbd59e3ef1043fb5
yuta20253 <magee.20253@gmail.com>
2025-09-02 13:43:04 +0900

    feat: accountDeleteアクションの実装

--- laravel/app/Http/Controllers/Api/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterFormRequest;
 use App\Models\User;
-
+use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     private User $user;
@@ -32,5 +32,14 @@ class RegisterController extends Controller
             'user' => $user,
             'token' => $token,
         ], 201);
+    }
+
+    public function accountDelete(Request $request)
+    {
+        $user = $request->user();
+        $user->update([
+            'email' => $user->maskedEmail(),
+        ]);
+        $user->delete();
     }
 }
