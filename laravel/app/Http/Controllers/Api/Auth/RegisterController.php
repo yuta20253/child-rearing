7c28f6bab2c11cbd74a67c4d60f7c912365686aa
yuta20253 <magee.20253@gmail.com>
2025-09-03 12:29:04 +0900

    style: Php-CS-Fixer 適用

--- laravel/app/Http/Controllers/Api/Auth/RegisterController.php
+++ laravel/app/Http/Controllers/Api/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterFormRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+
 class RegisterController extends Controller
 {
     private User $user;
