e3ec87950e9e31a68e5ff9094f4b65ddc6d58b98
yuta20253 <magee.20253@gmail.com>
2025-09-05 00:51:37 +0900

    add: Addressテーブルを作成・モデル作成

---
+++ laravel/app/Models/Address.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Facility;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'facility_id',
+        'municipality_id',
+        'town',
+        'chome',
+        'banchi',
+        'go',
+        'building',
+        'room',
+        'postal_code'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function facility()
+    {
+        return $this->belongsTo(Facility::class);
+    }
+
+    public function postalCodes()
+    {
+        return $this->hasMany(PostalCode::class);
+    }
+}
