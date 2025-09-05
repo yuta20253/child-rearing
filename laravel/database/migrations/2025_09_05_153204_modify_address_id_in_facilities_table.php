7291209d3f6b169e33cb3d3c553528d41886fc21
yuta20253 <magee.20253@gmail.com>
2025-09-06 00:58:10 +0900

    fix: addressカラムを削除してaddress_idをnull不可でカラムとして追加

---
+++ laravel/database/migrations/2025_09_05_153204_modify_address_id_in_facilities_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('facilities', function (Blueprint $table) {
+            $table->dropForeign(['address_id']);
+            $table->dropColumn('address_id');
+        });
+
+        Schema::table('facilities', function (Blueprint $table) {
+            $table->foreignId('address_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('facilities', function (Blueprint $table) {
+            $table->dropForeign(['address_id']);
+            $table->dropColumn('address_id');
+        });
+
+        Schema::table('facilities', function (Blueprint $table) {
+            $table->foreignId('address_id')
+                  ->nullable()
+                  ->constrained()
+                  ->cascadeOnDelete();
+        });
+    }
+};
