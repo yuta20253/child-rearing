a1b2b02afd230190444fe217c9f21f0d9a54cec3
yuta20253 <magee.20253@gmail.com>
2025-09-07 00:21:10 +0900

    feat: シーダデータの呼び出し設定

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\PrefectureSeeder;
+use Database\Seeders\MunicipalitySeeder;
+use Database\Seeders\AddressSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            PrefectureSeeder::class,
+            MunicipalitySeeder::class,
+            AddressSeeder::class,
             UserSeeder::class,
         ]);
     }
