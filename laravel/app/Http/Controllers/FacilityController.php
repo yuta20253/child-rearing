5e4ba5259d3f0cd07402cee16fdf35609b7f2945
yuta20253 <magee.20253@gmail.com>
2025-09-08 01:50:10 +0900

    add: indexアクションを追加

---
+++ laravel/app/Http/Controllers/FacilityController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\FacilityService;
+use Illuminate\Http\Request;
+
+class FacilityController extends Controller
+{
+    private FacilityService $facilityService;
+
+    public function __construct(FacilityService $facilityService)
+    {
+        $this->facilityService = $facilityService;
+    }
+
+    public function index ()
+    {
+        $facilities = $this->facilityService->getAllFacilities();
+        return response()->json([
+            "facilities" => $facilities,
+        ], 200);
+    }
+}
