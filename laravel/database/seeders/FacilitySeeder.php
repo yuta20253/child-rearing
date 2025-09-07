fdece0c0d116caabce09f88b9ca8a93a6160e14d
yuta20253 <magee.20253@gmail.com>
2025-09-07 17:08:58 +0900

    add: FacilitySeeder作成

---
+++ laravel/database/seeders/FacilitySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Facility;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FacilitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $addressIds = [90400, 90409, 90445, 90643];
+        $facilitiesArr = ['中央図書館', '北口図書館', '鳴尾図書館', '中央図書館上ヶ原分室'];
+        $latitudeArr = [34.730886, 34.74714, 34.719111, 34.755604];
+        $longitudeArr = [135.329875, 135.3578630, 135.364598, 135.338997];
+        $count = 0;
+
+        foreach($addressIds  as $index => $addressId) {
+            DB::table('facilities')->insert([
+                'name' => $facilitiesArr[$index],
+                'address_id' => $addressId,
+                'image' => '',
+                'latitude' => $latitudeArr[$index],
+                'longitude' => $longitudeArr[$index],
+                'equipment' => '図書館',
+                'description' => '西宮市にある図書館です。',
+            ]);
+            $count += 1;
+        }
+    }
+}
