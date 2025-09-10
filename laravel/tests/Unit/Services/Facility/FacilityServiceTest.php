90e202a757bc342d524aef60743f9bc902284b8e
yuta20253 <magee.20253@gmail.com>
2025-09-10 23:36:10 +0900

    fix: サービス層にUserのモックを作成し、addressと紐付け

--- laravel/tests/Unit/Services/Facility/FacilityServiceTest.php
+++ laravel/tests/Unit/Services/Facility/FacilityServiceTest.php
@@ -6,6 +6,7 @@ use App\Models\Facility;
 use App\Repositories\Facility\FacilityRepositoryInterface;
 use App\Services\FacilityService;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 
 class FacilityServiceTest extends TestCase
@@ -29,7 +30,21 @@ class FacilityServiceTest extends TestCase
             new Facility(['id' => 2, 'name' => 'Test施設2'])
         ]);
 
-        $this->facilityRepositoryMock->shouldReceive('getAllFacilities')->once()->andReturn($facilities);
+        // ダミーユーザーを作成
+        $user = new class {
+            public $address;
+            public function __construct() {
+                $this->address = new class {
+                    public $municipality_id = 123;
+                };
+            }
+        };
+
+        // Auth::user() をモック
+        Auth::shouldReceive('user')->once()->andReturn($user);
+
+
+        $this->facilityRepositoryMock->shouldReceive('getAllFacilities')->with(123)->once()->andReturn($facilities);
 
         $result = $this->facilityService->getAllFacilities();
 
