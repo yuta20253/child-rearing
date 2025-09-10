563fe9437eb950c75156f76df16e945d09dcb0ca
yuta20253 <magee.20253@gmail.com>
2025-09-10 23:57:37 +0900

    add: サービス層にfindメソッド実装

--- laravel/app/Services/FacilityService.php
+++ laravel/app/Services/FacilityService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\Facility;
 use App\Repositories\Facility\FacilityRepositoryInterface;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Support\Facades\Auth;
 
 class FacilityService
@@ -20,5 +21,10 @@ class FacilityService
         $user = Auth::user();
         $municipalityId = $user->address->municipality_id;
         return $this->facilityRepository->getAllFacilities($municipalityId);
+    }
+
+    public function find(int $id): Facility
+    {
+        return $this->facilityRepository->find($id);
     }
 }
