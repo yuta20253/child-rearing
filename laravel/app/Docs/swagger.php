2c2c7f4ab973b46dace571563322ae1dcd101e3e
yuta20253 <magee.20253@gmail.com>
2025-09-15 02:54:21 +0900

    add: L5-Swaggerの導入と設定

---
+++ laravel/app/Docs/swagger.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Docs;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="My API",
+ *     version="1.0.0",
+ *     description="APIドキュメントの説明"
+ * )
+ * @OA\Server(
+ *     url=L5_SWAGGER_CONST_HOST
+ * )
+ *
+ * @OA\Get(
+ *     path="/example",
+ *     summary="Example endpoint",
+ *     @OA\Response(
+ *         response=200,
+ *         description="OK"
+ *     )
+ * )
+ */
+class Swagger {}
