2432b6dcbb61f7732e292a6f4315188b409e9622
yuta20253 <magee.20253@gmail.com>
2025-10-02 01:07:43 +0900

    feat: indexアクションでweekを返す

--- laravel/app/Http/Controllers/MainController.php
+++ laravel/app/Http/Controllers/MainController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class MainController extends Controller
 {
+    public function index()
+    {
+        $week = $this->getWeek();
+        return response()->json(['week' => $week]);
+    }
 
+    private function getWeek()
+    {
+        Carbon::setLocale('ja');
+        $week = [];
+        for ($i=0; $i < 7; $i++) {
+            $date = Carbon::today()->addDays($i);
+            $week[] = [
+                'date' => $date->format('Y-m-d'),
+                'day' => $date->isoFormat('ddd'),
+            ];
+        }
+        return $week;
+    }
 }
