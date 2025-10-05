39a0d9994e7e1e3fb45491fb7e0d532c42e711b6
yuta20253 <magee.20253@gmail.com>
2025-10-06 02:36:55 +0900

    MainControllerのindexアクションにカレンダーとイベントとお気に入りを取得する

--- laravel/app/Http/Controllers/MainController.php
+++ laravel/app/Http/Controllers/MainController.php
@@ -3,19 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Services\CarenderService;
+use App\Services\FacilityFavoriteService;
+use App\Services\UserEventService;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     protected CarenderService $carenderService;
+    protected UserEventService $userEventService;
+    protected FacilityFavoriteService $facilityFavoriteService;
 
-    public function __construct(CarenderService $carenderService)
+    public function __construct(
+            CarenderService $carenderService,
+            UserEventService $userEventService,
+            FacilityFavoriteService $facilityFavoriteService
+        )
     {
         $this->carenderService = $carenderService;
+        $this->userEventService = $userEventService;
+        $this->facilityFavoriteService = $facilityFavoriteService;
     }
+
     public function index()
     {
-        $week = $this->carenderService->getCarender();
-        return response()->json(['week' => $week]);
+        $data['week'] = $this->carenderService->getCarender();
+        $data['events'] = $this->userEventService->getUserEvents();
+        $data['facilityFavorities'] = $this->facilityFavoriteService->getFacilityFavorities();
+
+        return response()->json(['data' => $data]);
     }
 }
