3205e7dc3cb7142fec5162ea9a39d23649cb7e1c
yuta20253 <magee.20253@gmail.com>
2025-10-06 02:13:48 +0900

    feat: トップページ画面作成

--- frontend/features/Home/index.tsx
+++ frontend/features/Home/index.tsx
@@ -1,22 +1,52 @@
 'use client'
 
+import { FavoriteFacilities } from '@/components/FavoriteFacility/FavoriteFacilities';
+import { TodayEvents } from '@/components/TodayEvent/TodayEvents';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { RequireAuth } from '@/components/RequireAuth';
 
 type WeekDay = {
   date: string;
   day: string;
 };
 
-export const Home = (): React.JSX.Element => {
-    const [week, SetWeek] = useState<WeekDay[]>([]);
-    console.log(week);
+type Event = {
+  id: number;
+  title: string;
+  description: string;
+  start_datetime: string;
+  end_datetime: string;
+  capacity: number;
+  memo: string;
+};
 
+type FacilityFavorite = {
+  id: number;
+  name: string;
+};
+
+export const Home = (): React.JSX.Element => {
+    const [week, setWeek] = useState<WeekDay[]>([]);
+    const [events, setEvents] = useState<Event[]>([]);
+    const [facilityFavorities, setFacilityFavorities] = useState<FacilityFavorite[]>([]);
+
+    facilityFavorities.map((facilityFavorite) => console.log(facilityFavorite));
     useEffect(() => {
         const fetchWeek = async () => {
+            const token = localStorage.getItem('token');
             try {
-                const res = await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL + '/api');
-                SetWeek(res.data.week);
+                const res = await axios.get(process.env.NEXT_PUBLIC_BACKEND_URL + '/api', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: 'application/json',
+                    },}
+
+                );
+                setWeek(res.data.data.week);
+                setEvents(res.data.data.events);
+                setFacilityFavorities(res.data.data.facilityFavorities)
+
             } catch (error) {
                 console.error('曜日取得エラー:', error);
             }
@@ -25,88 +55,58 @@ export const Home = (): React.JSX.Element => {
     }, [])
 
     return (
-        <div className="flex items-center justify-center  bg-gradient-to-tr">
-            <div className="relative w-3/4 sm:max-w-md md:max-w-lg lg:max-w-xl p-4 sm:p-2 flex flex-col space-y-2">
-                <div className="p-2">
-                    <div className="items-center justify-center rounded-lg">
-                        <p className="p-2 text-2xl text-center text-white bg-pink-200">子育てサポート</p>
-                    </div>
-                </div>
-                {/** カレンダー部分 */}
-                <div className="flex flex-col items-center justify-center rounded-lg mb-6">
-                    <p className="text-center font-bold">{week.length > 0 ? new Date(week[0].date).getMonth() + 1 : '' }月 カレンダー/予定登録</p>
-                    <div className="bg-yellow-50 m-4 p-4 rounded shadow w-full max-w-[700px] mx-auto overflow-x-auto">
-                        <table className="table-fixed border-collapse text-center w-full">
-                            <thead>
-                                <tr>
-                                    {week.map((day, i) => (
-                                    <th
-                                        key={i}
-                                        className="w-[14.2857%] min-w-[60px] px-3 py-2 whitespace-nowrap"
-                                    >
-                                        {day['day']}
-                                    </th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    {week.map((date, i) => (
-                                    <td key={i} className="px-2 py-4 h-12">
-                                        {
-                                            date['date'].slice(-2).replace(/^0/, "")
-                                        }
-                                    </td>
-                                    ))}
-                                </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-                {/** イベント部分 */}
-                <div className="mb-6">
-                    <p className="font-bold mb-3">今日の予定</p>
-                    <ul>
-                        <li className="flex w-full items-center space-x-4 p-2 rounded shadow h-12">
-                            <div className="text-blue-500 text-lg flex items-center justify-center w-10 h-10">Logo</div>
-                            <div className="flex flex-col justify-center">
-                                <p className="font-semibold">title</p>
-                                <p className="text-sm text-gray-500">予約</p>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                {/** お気に入り施設 */}
-                <div className="w-full max-w-[700px] mx-auto">
-                    <p className="font-bold mb-3">お気に入り施設</p>
-                    {
-                        <div className="overflow-x-auto pb-2">
-                            <div className="flex space-x-4 min-w-fit">
-                                <div className="flex-shrink-0 w-[240px] bg-white border border-gray-300 rounded p-4 shadow ">
-                                    <div className="flex flex-col justify-between pr-2">
-                                        <p className="font-semibold">施設名</p>
-                                        <p className="text-sm text-yellow-500">星評価</p>
-                                    </div>
-                                    <div>
-                                        <p className="text-sm text-gray-600">施設タイプ</p>
-                                        <p className="text-sm text-gray-600 mb-2">住所</p>
-                                    </div>
-                                    <div className="flex justify-end">
-                                        <button className="text-sm text-blue-600 hover:underline">詳細を見る</button>
-                                    </div>
-                                </div>
-                            </div>
+        <RequireAuth>
+            <div className="flex items-center justify-center  bg-gradient-to-tr">
+                <div className="relative w-3/4 sm:max-w-md md:max-w-lg lg:max-w-xl p-4 sm:p-2 flex flex-col space-y-2">
+                    <div className="p-2">
+                        <div className="items-center justify-center rounded-lg">
+                            <p className="p-2 text-2xl text-center text-white bg-pink-200 rounded-md">子育てサポート</p>
                         </div>
-                    }
-                </div>
-                <div className="w-full bg-pink-500 text-white p-4 rounded mt-6">
-                    <div className="flex justify-center">
-                        <button className="bg-white text-pink-500 font-semibold px-6 py-3 rounded">
-                            施設を探す
-                        </button>
+                    </div>
+                    {/** カレンダー部分 */}
+                    <div className="flex flex-col items-center justify-center rounded-lg mb-6">
+                        <p className="text-center font-bold">{week.length > 0 ? new Date(week[0].date).getMonth() + 1 : '' }月 カレンダー/予定登録</p>
+                        <div className="bg-yellow-50 m-4 p-4 rounded shadow w-full max-w-[700px] mx-auto overflow-x-auto">
+                            <table className="table-fixed border-collapse text-center w-full">
+                                <thead>
+                                    <tr>
+                                        {week.map((day, i) => (
+                                        <th
+                                            key={i}
+                                            className="w-[14.2857%] min-w-[60px] px-3 py-2 whitespace-nowrap"
+                                        >
+                                            {day['day']}
+                                        </th>
+                                        ))}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        {week.map((date, i) => (
+                                        <td key={i} className="px-2 py-4 h-12">
+                                            {
+                                                date['date'].slice(-2).replace(/^0/, "")
+                                            }
+                                        </td>
+                                        ))}
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    {/** イベント部分 */}
+                    <TodayEvents todayEvents={events} />
+                    {/** お気に入り施設 */}
+                    <FavoriteFacilities facilityFavorities={facilityFavorities} />
+                    <div className="w-full bg-pink-300 text-white p-2 rounded mt-6">
+                        <div className="flex justify-center">
+                            <button className="font-semibold px-6 py-2 rounded">
+                                施設を探す
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </RequireAuth>
     );
 };
