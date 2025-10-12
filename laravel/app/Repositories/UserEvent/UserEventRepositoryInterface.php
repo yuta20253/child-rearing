b40b76fc1bf9cb551ae3e610bc99e7dfe8c28b31
yuta20253 <magee.20253@gmail.com>
2025-10-13 00:07:43 +0900

    refactor: PSR12の適用

--- laravel/app/Repositories/UserEvent/UserEventRepositoryInterface.php
+++ laravel/app/Repositories/UserEvent/UserEventRepositoryInterface.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface UserEventRepositoryInterface
 {
-    public function getUserEvents(int $id, ?DateTime $targetDate ): Collection;
+    public function getUserEvents(int $id, ?DateTime $targetDate): Collection;
 }
