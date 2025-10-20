152f55d3030faa892b924e1985aca583e2f94be8
yuta20253 <magee.20253@gmail.com>
2025-10-20 22:26:38 +0900

    remove: 不要なimportを削除

--- frontend/features/Facilities/Map/index.tsx
+++ frontend/features/Facilities/Map/index.tsx
@@ -6,7 +6,6 @@ import L from 'leaflet';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
-import 'leaflet/dist/leaflet.css';
 import { Facility } from '@/types/generated/api';
 
 delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: unknown })._getIconUrl;
