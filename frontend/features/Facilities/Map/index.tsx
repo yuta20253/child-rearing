49f95c67890773b0992a5b42d94917cc736873e6
yuta20253 <magee.20253@gmail.com>
2025-10-24 01:01:33 +0900

    fix: 地図の画像をpublicに配下に移動

--- frontend/features/Facilities/Map/index.tsx
+++ frontend/features/Facilities/Map/index.tsx
@@ -2,17 +2,12 @@
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
-import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
-import markerIcon from 'leaflet/dist/images/marker-icon.png';
-import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 import { Facility } from '@/types/generated/api';
 
-delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: unknown })._getIconUrl;
-
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: markerIcon2x.src,
-  iconUrl: markerIcon.src,
-  shadowUrl: markerShadow.src,
+  iconRetinaUrl: '/images/leaflet/marker-icon-2x.png',
+  iconUrl: '/images/leaflet/marker-icon.png',
+  shadowUrl: '/images/leaflet/marker-shadow.png',
 });
 
 type MapProps = {
