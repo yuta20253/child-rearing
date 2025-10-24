6b6090354f8d9533e2fc99b88cf98cdc9987e260
yuta20253 <magee.20253@gmail.com>
2025-10-24 19:25:23 +0900

    importの行を変更

--- frontend/features/Facilities/hooks.ts
+++ frontend/features/Facilities/hooks.ts
@@ -1,10 +1,10 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 type FormDataType = {
   name: string;
 };
-
-import { useRouter } from 'next/navigation';
 
 export const useSubmit = () => {
   const router = useRouter();
