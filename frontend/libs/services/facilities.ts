70d21e9886622a5f5522c73bed0b70b618a33ede
yuta20253 <magee.20253@gmail.com>
2025-10-29 00:40:56 +0900

    Laravelとの連携箇所

--- frontend/libs/services/facilities.ts
+++ frontend/libs/services/facilities.ts
@@ -1,5 +1,5 @@
 import { Facility } from '@/types/generated/api';
-import { facilities } from './apiClient';
+import { facilities, facility } from './apiClient';
 
 export const getFacilities = async (token: string, name?: string): Promise<Facility[] | null> => {
   try {
@@ -20,5 +20,33 @@ export const getFacilities = async (token: string, name?: string): Promise<Facil
   } catch (error) {
     console.error(error);
     throw new Error('施設一覧の取得に失敗しました');
+  }
+};
+
+export const getFacility = async (token: string, id: number): Promise<Facility> => {
+  try {
+    const response = await facility.facilityInfo(
+      id,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const data = response.data.facility;
+
+    if (!data) throw new Error('施設データが存在しません');
+
+    return data;
+  } catch (error) {
+    console.log(error);
+      const message =
+        error instanceof Error
+          ? error.message
+          : typeof error === 'string'
+            ? error
+            : '不明なエラーが発生しました';
+    throw new Error(message);
   }
 };
