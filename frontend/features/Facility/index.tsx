0e4bd509951c73ec824b4c061d74a5e5362dc4cd
yuta20253 <magee.20253@gmail.com>
2025-10-30 00:06:27 +0900

    施設詳細画面に口コミと住所、設備情報追加

--- frontend/features/Facility/index.tsx
+++ frontend/features/Facility/index.tsx
@@ -3,17 +3,17 @@
 import { RequireAuth } from "@/components/RequireAuth";
 import { useState, useEffect } from "react";
 import dynamic from 'next/dynamic';
-import { Facility } from "@/types/generated/api";
+import { FacilityWithRelations } from "@/types/generated/api";
 import { getFacility } from "@/libs/services/facilities";
 
 const Map = dynamic(() => import('./Map').then(mod => mod.Map), { ssr: false });
 
 export const FacilityPage = ({ id }: {id: string}): React.JSX.Element => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [facility, setFacility] = useState<Facility | undefined>(undefined);
+    const [facility, setFacility] = useState<FacilityWithRelations | undefined>(undefined);
 
+    console.log(facility);
     useEffect(() => {
-        console.log('id', id);
         const facilityId = Number(id);
         if (isNaN(facilityId)) return console.error('無効な施設IDです');
         const token = localStorage.getItem('token');
@@ -52,26 +52,30 @@ export const FacilityPage = ({ id }: {id: string}): React.JSX.Element => {
                             <p className="text-2xl">🏠 {facility.name}</p>
                         </div>
                         <div className="mt-6 w-full max-w-md mx-auto space-y-4">
-                            <div className="flex justify-between bg-white shadow rounded p-4">
-                                <div className="w-1/2 pr-2">
-                                    <div className="font-semibold">住所</div>
-                                    <div></div>
+                            <div className="justify-between p-4">
+                                <div className="">
+                                    <div className="">住所</div>
+                                    <div className="pl-2">{facility.address?.municipality?.prefecture?.name || '' + facility.address?.municipality?.name + facility.address?.town}</div>
                                 </div>
-                                <div className="w-1/2 pl-2">
-                                    <div className="font-semibold">電話番号</div>
-                                    <div></div>
-                                </div>
-                            </div>
-                            <div className="flex justify-between bg-white shadow rounded p-4">
-                                <div className="w-1/2 pr-2">
-                                    <div className="font-semibold">営業時間</div>
-                                    <div></div>
-                                </div>
-                                <div className="w-1/2 pl-2">
-                                    <div className="font-semibold">設備情報</div>
-                                    <div></div>
+                                <div className="">電話番号</div>
+                                <div className="">営業時間</div>
+                                <div className="">
+                                    <div className="">設備情報</div>
+                                    <div className="pl-2">{facility.equipment}</div>
                                 </div>
                             </div>
+                        </div>
+                        <div>
+                            <p>💬口コミ</p>
+                            {
+                                facility.reviews?.map((review) => (
+                                    <div key={review.id}>
+                                        <p>{review.rating}</p>
+                                        <div>{review.user?.name}</div>
+                                        <div>{review.comment}</div>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
                 ) : (
