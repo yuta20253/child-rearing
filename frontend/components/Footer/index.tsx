12ba7f0a6816bbab929a198619d232c526a0e1f2
yuta20253 <magee.20253@gmail.com>
2025-11-28 23:16:13 +0900

    アイコンにリンクをつけた

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -5,18 +5,28 @@ import { IoMdHome } from 'react-icons/io';
 import { TbBuildingStore } from 'react-icons/tb';
 import { MdFavoriteBorder } from 'react-icons/md';
 import { IoMdPerson } from 'react-icons/io';
+import Link from 'next/link';
 
 export const Footer = (): React.JSX.Element => {
   const { user } = useAuthState();
 
   if (!user) return <></>;
+
   return (
     <div className="fixed bottom-0 left-0 w-full bg-pink-200 z-20">
       <div className="flex items-center justify-between min-h-[64px] px-6 max-w-6xl mx-auto">
-        <IoMdHome color="#ffffff" size="2.2em" />
-        <TbBuildingStore color="#ffffff" size="2.2em" />
-        <MdFavoriteBorder color="#ffffff" size="2.2em" />
-        <IoMdPerson color="#ffffff" size="2.2em" />
+        <Link href="/">
+          <IoMdHome color="#ffffff" size="2.2em" />
+        </Link>
+        <Link href="/facilities">
+          <TbBuildingStore color="#ffffff" size="2.2em" />
+        </Link>
+        <Link href="/favorites">
+          <MdFavoriteBorder color="#ffffff" size="2.2em" />
+        </Link>
+        <Link href="/mypage">
+          <IoMdPerson color="#ffffff" size="2.2em" />
+        </Link>
       </div>
     </div>
   );
