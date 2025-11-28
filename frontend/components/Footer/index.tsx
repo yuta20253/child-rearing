5250b36532d7426fb25a8bacd778dfd181e0cc50
yuta20253 <magee.20253@gmail.com>
2025-11-28 22:53:47 +0900

    アイコンのサイズを変更

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -13,10 +13,10 @@ export const Footer = (): React.JSX.Element => {
   return (
     <div className="fixed bottom-0 left-0 w-full bg-pink-200 z-20">
       <div className="flex items-center justify-between min-h-[64px] px-6 max-w-6xl mx-auto">
-        <IoMdHome color="#ffffff" size="1.5em" />
-        <TbBuildingStore color="#ffffff" size="1.5em" />
-        <MdFavoriteBorder color="#ffffff" size="1.5em" />
-        <IoMdPerson color="#ffffff" size="1.5em" />
+        <IoMdHome color="#ffffff" size="2.2em" />
+        <TbBuildingStore color="#ffffff" size="2.2em" />
+        <MdFavoriteBorder color="#ffffff" size="2.2em" />
+        <IoMdPerson color="#ffffff" size="2.2em" />
       </div>
     </div>
   );
