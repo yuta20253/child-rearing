784ae13fc2117d7f55e2deb47dfb6e869c004dc0
yuta20253 <magee.20253@gmail.com>
2025-12-15 22:56:17 +0900

    handleDateActionメソッドにsetCurrentMonthの処理を移した

--- frontend/features/Home/Calender/hook.ts
+++ frontend/features/Home/Calender/hook.ts
@@ -1,32 +1,27 @@
 import { SetStateAction } from 'react';
-import { Event as ApiEvent } from '@/types/generated/api';
-import axios from 'axios';
 import type { DatesSetArg } from '@fullcalendar/core';
 import { DateClickArg } from '@fullcalendar/interaction';
 
 type Props = {
-  token: string | null;
-  setEvents: (value: SetStateAction<ApiEvent[]>) => void;
   setSelectedDate: (value: SetStateAction<string>) => void;
+  setCurrentMonth: (value: SetStateAction<YearMonth>) => void;
 };
 
-export const handleDateAction = ({ token, setEvents, setSelectedDate }: Props) => {
+type YearMonth = {
+  year: number;
+  month: number;
+};
+
+export const handleDateAction = ({ setSelectedDate, setCurrentMonth }: Props) => {
   const handleDatesSet = async (data: DatesSetArg) => {
     const year = data.view.currentStart.getFullYear();
     const month = data.view.currentStart.getMonth() + 1;
 
-    try {
-      const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/calendar/events`, {
-        params: { year, month },
-        headers: {
-          Authorization: `Bearer ${token}`,
-          Accept: 'application/json',
-        },
-      });
-      setEvents(res.data.events);
-    } catch (error) {
-      console.error('月切り替えイベント取得エラー:', error);
-    }
+    setCurrentMonth(prev => {
+      if (prev.year === year && prev.month === month) return prev;
+
+      return { year, month };
+    })
   };
 
   const handleChangeDate = (data: DateClickArg) => {
