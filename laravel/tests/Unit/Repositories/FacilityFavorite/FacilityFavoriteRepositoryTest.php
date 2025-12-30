9c8a4179c36a6a73b5bcc6a48167845f4e15ea6d
yuta20253 <magee.20253@gmail.com>
2025-12-31 03:43:12 +0900

    テスト追加

---
+++ laravel/tests/Unit/Repositories/FacilityFavorite/FacilityFavoriteRepositoryTest.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Tests\Unit\Repositories\FacilityFavorite;
+
+use App\Models\Facility;
+use App\Models\User;
+use App\Repositories\FacilityFavorite\FacilityFavoriteRepository;
+use App\Repositories\FacilityFavorite\FacilityFavoriteRepositoryInterface;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class FacilityFavoriteRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private FacilityFavoriteRepositoryInterface $facilityFavoriteRepository;
+    /**
+     * A basic unit test example.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        app()->bind(FacilityFavoriteRepositoryInterface::class, FacilityFavoriteRepository::class);
+        $this->facilityFavoriteRepository = app(FacilityFavoriteRepositoryInterface::class);
+    }
+
+    /** @test */
+    public function getUserFacilityFavoritiesはそのユーザーの施設だけ返す(): void
+    {
+        $user1 = User::factory()->create();
+        $user2 = User::factory()->create();
+
+        $facility1 = Facility::factory()->create();
+        $facility2 = Facility::factory()->create();
+
+        $user1->facilityFavorities()->attach($facility1->id);
+        $user2->facilityFavorities()->attach($facility2->id);
+
+        $result = $this->facilityFavoriteRepository->getUserFacilityFavorities($user1->id);
+
+        $this->assertCount(1, $result);
+        $this->assertEquals($facility1->id, $result->first()->id);
+    }
+
+    /**
+     * @test
+     */
+    // phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function registerUserFacilityFavoriteで中間テーブルにレコードが追加されること(): void
+    {
+        $user = User::factory()->create();
+        $facility = Facility::factory()->create();
+
+        $this->facilityFavoriteRepository->registerUserFacilityFavorite($facility->id, $user->id);
+
+        $this->assertDatabaseHas('facility_favorites', [
+            'user_id' => $user->id,
+            'facility_id' => $facility->id,
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    // phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function cancelUserFacilityFavoriteで中間テーブルにレコードが削除されること(): void
+    {
+        $user = User::factory()->create();
+        $facility = Facility::factory()->create();
+
+        $this->facilityFavoriteRepository->cancelUserFacilityFavorite($facility->id, $user->id);
+
+        $this->assertDatabaseMissing('facility_favorites', [
+            'user_id' => $user->id,
+            'facility_id' => $facility->id,
+        ]);
+    }
+}
---
+++ laravel/tests/Unit/Services/FacilityFavorite/FacilityFavoriteServiceTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Tests\Unit\Services\FacilityFavorite;
+
+use App\Models\Facility;
+use App\Repositories\FacilityFavorite\FacilityFavoriteRepositoryInterface;
+use App\Services\FacilityFavoriteService;
+use Barryvdh\LaravelIdeHelper\Eloquent;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use Illuminate\Support\Facades\Auth;
+use Tests\TestCase;
+
+class FacilityFavoriteServiceTest extends TestCase
+{
+    /** @var FacilityFavoriteRepositoryInterface&\Mockery\MockInterface $facilityFavoriteRepositoryMock */
+    private $facilityFavoriteRepositoryMock;
+    private FacilityFavoriteService $facilityFavoriteService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->facilityFavoriteRepositoryMock = $this->mock(FacilityFavoriteRepositoryInterface::class);
+        $this->facilityFavoriteService = new FacilityFavoriteService($this->facilityFavoriteRepositoryMock);
+    }
+
+    /**
+     * @test
+     */
+    // phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function registerFacilityFavoriteメソッドが認証ユーザーidでRepositoryを呼べること(): void
+    {
+        Auth::shouldReceive('id')->once()->andReturn(1);
+        $this->facilityFavoriteRepositoryMock
+             ->shouldReceive('registerUserFacilityFavorite')
+             ->once()
+             ->with(123, 1);
+
+        $this->facilityFavoriteService->registerFacilityFavorite(123);
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @test
+     */
+    // phpcs:disable PSR1.Methods.CamelCapsMethodName.NotCamelCaps
+    public function cancelFacilityFavoriteメソッドが認証ユーザーidでRepositoryを呼べること(): void
+    {
+        Auth::shouldReceive('id')->once()->andReturn(1);
+        $this->facilityFavoriteRepositoryMock
+             ->shouldReceive('cancelUserFacilityFavorite')
+             ->once()
+             ->with(123, 1);
+
+        $this->facilityFavoriteService->cancelFacilityFavorite(123);
+        $this->assertTrue(true);
+    }
+}
