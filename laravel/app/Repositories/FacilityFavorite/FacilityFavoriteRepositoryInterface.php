8fe33fdb8948f224493d13d1e2f319200180a518
yuta20253 <88157935+yuta20253@users.noreply.github.com>
2026-01-05 23:07:38 +0900

    Update laravel/app/Repositories/FacilityFavorite/FacilityFavoriteRepositoryInterface.php
    Co-authored-by: Ryo Nakadoi <42470564+centerRyo@users.noreply.github.com>

--- laravel/app/Repositories/FacilityFavorite/FacilityFavoriteRepositoryInterface.php
+++ laravel/app/Repositories/FacilityFavorite/FacilityFavoriteRepositoryInterface.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Collection;
 
 interface FacilityFavoriteRepositoryInterface
 {
-    public function getUserFacilityFavorities(int $id): Collection;
+    public function getUserFacilityFavorites(int $id): Collection;
     public function registerUserFacilityFavorite(int $facilityId, int $userId): bool;
     public function cancelUserFacilityFavorite(int $facilityId, int $userId): bool;
 }
