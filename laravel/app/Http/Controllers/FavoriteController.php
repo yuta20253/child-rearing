5cec76d89974fbe2b6a7a823b92bb9a73f9e3463
yuta20253 <magee.20253@gmail.com>
2026-02-01 18:37:41 +0900

    swagger情報記述

--- laravel/app/Http/Controllers/FavoriteController.php
+++ laravel/app/Http/Controllers/FavoriteController.php
@@ -6,6 +6,49 @@ use App\Services\FacilityService;
 use App\Services\FacilityFavoriteService;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\POST(
+ *     path="/api/facilities/{facilityId}/favorite",
+ *     summary="施設お気に入り登録",
+ *     operationId="RegisterFacilityFavorite",
+ *     description="施設お気に入り登録",
+ *     @OA\Parameter(
+ *         name="facilityId",
+ *         in="path",
+ *         required=true,
+ *         description="施設ID",
+ *         @OA\Schema(type="integer", example=1)
+ *     ),
+ *     @OA\Response(
+ *        response=201,
+ *        description="施設お気に入り登録成功",
+ *        @OA\JsonContent(
+ *           @OA\Property(property="message", type="string", example="お気に入り登録しました。")
+ *        )
+ *     )
+ * )
+ *
+ * @OA\DELETE(
+ *     path="/api/facilities/{facilityId}/favorite",
+ *     summary="施設お気に入り解除",
+ *     operationId="CancelFacilityFavorite",
+ *     description="施設お気に入り解除",
+ *     @OA\Parameter(
+ *         name="facilityId",
+ *         in="path",
+ *         required=true,
+ *         description="施設ID",
+ *         @OA\Schema(type="integer", example=1)
+ *     ),
+ *     @OA\Response(
+ *        response=201,
+ *        description="施設お気に入り解除成功",
+ *        @OA\JsonContent(
+ *           @OA\Property(property="message", type="string", example="お気に入り解除しました。")
+ *        )
+ *     )
+ * )
+ */
 class FavoriteController extends Controller
 {
     protected FacilityFavoriteService $facilityFavoriteService;
